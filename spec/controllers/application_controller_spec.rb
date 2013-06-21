c64c666710f86e7556f77c776ed2be8f14ce5f86
K Eric Mittler <eric_mittler@mac.com>
2013-06-21 13:28:16 -0700

    test coverage for ApplicationController

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,8 +2,9 @@ require 'spec_helper'
 
 
 class RspecTestingStubController < ApplicationController
+  before_filter :ensure_authenticated
   def some_get_method
-    render :nothing => true
+    render :text => 'successfully rendered'
   end
 end
 
@@ -13,10 +14,15 @@ describe RspecTestingStubController do
   let(:user) { FactoryGirl.create(:user) }
   
   describe 'force_ssl' do
+    before :each do
+      session[:user_id] = user.id
+    end
+    
     it 'should not be successful when ssl is on' do
       request.env['HTTPS'] = 'on'
       get :some_get_method
       response.should be_successful
+      response.body.should == 'successfully rendered'
     end
     
     it 'should not be successful when ssl is off' do
@@ -51,6 +57,34 @@ describe RspecTestingStubController do
     it 'should return false when a user has no authenticated' do
       session[:user_id] = nil
       controller.send(:logged_in?).should be_false
+    end
+  end
+  
+  describe 'ensure_authenticated' do
+    before :each do
+      request.env['HTTPS'] = 'on'
+    end
+    
+    context 'when user has authenticated' do
+      it 'should do nothing and allow expected template to render' do
+        session[:user_id] = user.id
+        get :some_get_method
+        response.should be_successful
+        response.body.should == 'successfully rendered'
+      end
+    end
+    
+    context 'when user has not authenticated' do
+      before :each do
+        session[:user_id] = nil
+      end
+      
+      it 'should redirect to new_session_url' do
+        get :some_get_method
+        response.should redirect_to new_session_url
+      end
+      
+      it 'should remember the desired_url'
     end
   end
 end
